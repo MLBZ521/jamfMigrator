8c2db54cd186d993b108c5021785491920a98fb4
Zack T <MLBZ521@gmail.com>
2017-11-26 15:08:54 -0700

    v0.5 = Numerous updates and fixes / Updated naming convention
     + Tweaked usage of exitStatus Function
     + Corrected logic in the checkJSSConnection Function
     + Created Function for the enroll package step
     + Modified the curl command parameters

--- jamfMigrator.sh
+++ jamfMigrator.sh
@@ -3,13 +3,13 @@
 ###################################################################################################
 # Script Name:  jamfMigrator.sh
 # By:  Zack Thompson / Created:  11/17/2017
-# Version:  0.4 / Updated:  11/20/2017 / By:  ZT
+# Version:  0.5 / Updated:  11/21/2017 / By:  ZT
 #
 # Description:  This script uses the Jamf API to mark the device as unmanaged, remove the current Jamf Framework, then install a new QuickAdd package, and finally cleanup after itself.
 #
 ###################################################################################################
 
-/bin/echo "Starting jamfMigrator script..."
+/usr/bin/logger -s "*****  jssMigrator process:  START  *****"
 
 ##################################################
 # Define Variables
@@ -18,6 +18,7 @@
 	jamfAPIUser="APIUsername"
 	jamfAPIPassword="APIPassword"
 	jamfURL="${oldJSS}/JSSResource/computers/udid/"
+	jamfBinary="/usr/local/bin/jamf"
 	getUUID=$(/usr/sbin/ioreg -rd1 -c IOPlatformExpertDevice | /usr/bin/awk '/IOPlatformUUID/ { split($0, line, "\""); printf("%s\n", line[4]); }')
 	unmanagePayload="/private/tmp/unmanage_UUID.xml"
 	launchDaemonLabel="com.github.mlbz521.jamfMigrator"
@@ -38,51 +39,84 @@
 # Setup Functions
 
 	function checkJSSConnection {
-		checkAvailablity=$(/usr/local/bin/jamf checkJSSConnection)
-			# Function exitStatus
-				exitStatus $? "Unable to run \`jamf checkJSSConnection\`"
+		if [[ -e $jamfBinary ]]; then
+			/usr/bin/logger -s "Checking if current JSS instance is available..."
+			checkAvailablity=$(${jamfBinary} checkJSSConnection)
+				# Function exitStatus
+					exitStatus $? "${1} is unavailable at this time.  Suspending until next interval..."
 
-		if [[ $checkAvailablity == *"${1}"* ]]; then
-			if ![[ $checkAvailablity == *"The JSS is available"* ]]; then
-				/bin/echo "${1} is unavailable at this time.  Suspending until next interval..."
+			if [[ $checkAvailablity == *"${1}"* ]]; then
+				# If the check contains the JSS we're expecting...
+				if [[ $checkAvailablity != *"The JSS is available"* ]]; then
+					# If the JSS is unavailable, suspend further processing...
+					/usr/bin/logger -s "${1} is unavailable at this time.  Suspending until next interval..."
+					exit 1
+				else
+					# If the JSS is available, then continue...
+					/usr/bin/logger -s "${1} is available, continuing..."
+				fi
+			elif [[ $checkAvailablity == *"${oldJSS}"* ]]; then
+				# If the check is the oldJSS when we're expecting the newJSS, something went wrong, suspend further processing... 
+				/usr/bin/logger -s "Failed -- still pointing to the old JSS Server...  Suspending until next interval..."
 				exit 1
-			else
-				/bin/echo "${1} is available, continuing..."
+			elif [[ $checkAvailablity == *"${newJSS}"* ]]; then
+				# This elif is for, if somehow on the first checkJSSConnection run, it's connected to the new JSS, then we're good to go.
+				/usr/bin/logger -s "Connected to the new JSS instance!"
+				exit 0
+				/usr/bin/logger -s "*****  jssMigrator process:  COMPLETE  *****"
 			fi
-		elif [[ $checkAvailablity == $oldJSS ]]; then
-			/bin/echo "Failed -- still pointing to the old JSS Server...  Suspending until next interval..."
-			exit 1
+		else
+			/usr/bin/logger -s "Unable to run \`jamf checkJSSConnection\`"
+			/usr/bin/logger -s "Assuming Jamf Framework has been removed..."
+			# Function enrollMachine
+				enrollMachine
 		fi
-
 	}
 
-	function exitStatus {
-		if [[ $1 != "0" ]]; then
-			/bin/echo "Failed at stage:  ${2}"
-			exit $1
-		fi
+	function enrollMachine {
+		# Enroll machine
+			/usr/bin/logger -s "Installing QuickAdd package"
+			/usr/sbin/installer -dumplog -verbose -pkg $enrollPkg -allowUntrusted -target /
+				# Function exitStatus
+					exitStatus $?
 	}
 
 	function tearDown {
 		# Unload LaunchDaemon
+			/usr/bin/logger -s "Unloading LaunchDaemon"
 			/bin/launchctl unload $launchDaemonLocation
 				# Function exitStatus
-					exitStatus $? "Unloading LaunchDaemon"
+					exitStatus $?
 
 		# Remove LaunchDaemon
+			/usr/bin/logger -s "Deleting LaunchDaemon"
 			/bin/rm -f $launchDaemonLocation
 				# Function exitStatus
-					exitStatus $? "Deleting LaunchDaemon"
+					exitStatus $?
 
 		# Delete QuickAdd Package
+			/usr/bin/logger -s "Deleting QuickAdd Package"
 			/bin/rm -f $enrollPkg
 				# Function exitStatus
-					exitStatus $? "Deleting LaunchDaemon"
+					exitStatus $?
 
 		# Delete Self
+			/usr/bin/logger -s "Deleting Script"
 			/bin/rm -f "$0"
 				# Function exitStatus
-					exitStatus $? "Deleting Script"
+					exitStatus $?
+	}
+
+	function exitStatus {
+		if [[ $1 != "0" ]]; then
+			/usr/bin/logger -s " -> Failed"
+				if [[ -e $2 ]]; then
+					/usr/bin/logger -s "Error:  ${2}"
+				fi
+				exit 1
+		else
+			/usr/bin/logger -s " -> Success!"
+		fi
 	}
 
 ##################################################
@@ -91,30 +125,27 @@
 # Function checkJSSConnection
 	checkJSSConnection $oldJSS
 
-# Submit unamange payload to the JSS
-/usr/bin/curl -sfkuf "${jamfAPIUser}:${jamfAPIPassword}" "${jamfURL}/${getUUID}" -T $unmanagePayload -X PUT
-	# Function exitStatus
-		exitStatus $? "Sending API Payload to Unmanage Computer"
-
-# Pause for a moment to get a response back from the JSS...
-	# /bin/sleep 5
+# Submit unamange payload to the JSS (add -k, --insecure to disabled SSL verification)
+	/usr/bin/logger -s "Sending API Payload to Unmanage Computer"
+	/usr/bin/curl --silent --show-error --fail --user "${jamfAPIUser}:${jamfAPIPassword}" "${jamfURL}/${getUUID}" --header "Content-Type: text/xml" --upload-file $unmanagePayload --request PUT
+		# Function exitStatus
+		exitStatus $?
 
 # Remove JAMF Binary
-	/usr/local/bin/jamf removeFramework
+	/usr/bin/logger -s "Removing Framework"
+	$jamfBinary removeFramework
 		# Function exitStatus
-			exitStatus $? "Removing Framework"
+			exitStatus $?
 
-# Enroll machine
-	/usr/sbin/installer -dumplog -verbose -pkg $enrollPkg -allowUntrusted -target /
-		# Function exitStatus
-			exitStatus $? "Sending API Payload to Unmanage Computer"
+# Function enrollMachine
+	enrollMachine
 
 # Function checkJSSConnection
 	checkJSSConnection $newJSS
 
-
 # Function tearDown
 	tearDown
 
-exit 0
+/usr/bin/logger -s "*****  jssMigrator process:  COMPLETE  *****"
 
+exit 0
