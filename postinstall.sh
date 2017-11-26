003c686fa533ae4026545122a742698b7b7d2e89
Zack T <MLBZ521@gmail.com>
2017-11-25 19:23:42 -0700

    v0.2 = Adding staging to postinstall
     + Handling all the staging in the postinstall script now.

--- postinstall.sh
+++ postinstall.sh
@@ -3,15 +3,21 @@
 ###################################################################################################
 # Script Name:  postinstall.sh
 # By:  Zack Thompson / Created:  11/20/2017
-# Version:  0.1 / Updated:  11/20/2017 / By:  ZT
+# Version:  0.2 / Updated:  11/21/2017 / By:  ZT
 #
-# Description:  This script loads the LaunchDaemon.
+# Description:  This script stages files and loads a LaunchDaemon.
 #
 ###################################################################################################
 
 # Define the Variables
+	pkgDir=$(/usr/bin/dirname $0)
 	launchDaemonLabel="com.github.mlbz521.jamfMigrator"
 	launchDaemonLocation="/Library/LaunchDaemons/${launchDaemonLabel}.plist"
+
+# Stages the bits
+	cp "${pkgDir}/${launchDaemonLabel}.plist" $launchDaemonLocation
+	cp -R "${pkgDir}/jamfMigrator.sh" /private/var/tmp/
+	cp -R "${pkgDir}/QuickAdd.pkg" /private/var/tmp/
 
 # Load the LaunchDaemon
 	/bin/launchctl bootstrap system $launchDaemonLocation
